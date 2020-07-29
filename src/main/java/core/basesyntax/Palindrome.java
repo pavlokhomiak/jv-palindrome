7551874e266a35b1361375ac86eea0168a3ce48d
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-07-29 15:12:14 +0300

    Add solution

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,7 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        return new StringBuilder(text.toUpperCase().replaceAll("[\\W]","")).reverse().toString()
+                .equals(text.toUpperCase().replaceAll("[\\W]",""));
     }
 }
